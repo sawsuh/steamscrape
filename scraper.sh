5c3a46cb17c826063665a55586bf2e8c7addbe7d
sawsuh <prao060@gmail.com>
2020-02-15 02:11:35 +1100

    small updates

--- scraper.sh
+++ scraper.sh
@@ -1,6 +1,6 @@
 cd steam
 rm out.json
-timeout 60 scrapy crawl steamscrape -o out.json -t json
+scrapy crawl steamscrape -o out.json -t json
 python jsonInterpret.py > ../out.txt
 cd ..
 nvim out.txt
