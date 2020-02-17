7693cd1c20d99cc244d28fefcc0956922173ed68
sawsuh <prao060@gmail.com>
2020-02-17 17:15:06 +1100

    made sorting a bit nicer

--- steam/jsonInterpret.py
+++ steam/jsonInterpret.py
@@ -1,4 +1,5 @@
 import json
+import math
 
 with open('out.json') as f:
     data = json.load(f)
@@ -11,19 +12,21 @@ for game in data:
         goodreviews = float(game["posreviews"].replace(',',''))
         posprop = 100*goodreviews/totalreviews
     except ValueError:
-        posprop = None
-        pass
+        #posprop = None
+        continue
     try:
         discountprice = float(game["discountprice"][3:])
         oldprice = float(game["oldprice"][3:])
         fraction = 100*(1 - discountprice/oldprice)
+        group = math.floor(fraction/10)
     except ValueError:
         continue
-    tuplist.append((title, fraction, posprop, discountprice))
+    tuplist.append((title, group, fraction, posprop, discountprice))
 
-newlist = sorted(tuplist, key = lambda x : x[1], reverse=True)
-for title, frac, posprop, p in newlist:
-    if posprop:
-        print(f'{title} - ${p} {posprop:.2f}% ({frac:.2f}% off)')
-    else:
-        print(f'{title} - ${p} ({frac:.2f}% off)')
+newlistIntermediate = sorted(tuplist, key = lambda x : x[3], reverse=True)
+newlist = sorted(newlistIntermediate, key = lambda x : x[1], reverse=True)
+for title, g, frac, posprop, p in newlist:
+    #if posprop:
+    print(f'{title} - ${p} {posprop:.2f}% ({frac:.2f}% off)')
+    #else:
+        #print(f'{title} - ${p} ({frac:.2f}% off)')
